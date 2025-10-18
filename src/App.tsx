09c74a26b41cb751bed70da1f3c4a615c5f34a16
Deep <diphiwase0987@gmail.com>
2025-10-18 16:42:40 +0530

    Add fevicon, meta tags like title, description, keywords and google fonts Inter and add it to tailwind primary custom font

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 const App = () => {
   return (
     <div>
+      <p className="text-2xl text-teal-300 font-primary">Hello World!</p>
       <p className="text-2xl text-teal-300">Hello World!</p>
       <Button>Click Me!</Button>
     </div>
